dd53bac8ab99af9a09f5c7773798c8e494f72c67
Rob Fellows <rfellows@pentaho.com>
2016-03-24 17:08:19 -0400

    [BACKLOG-7286] - Adding browser controls (back, forward, go, and url bar)

--- ui/src/org/pentaho/di/ui/spoon/SpoonJavaFxBrowser.java
+++ ui/src/org/pentaho/di/ui/spoon/SpoonJavaFxBrowser.java
@@ -3,13 +3,20 @@ package org.pentaho.di.ui.spoon;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.embed.swt.FXCanvas;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import javafx.scene.web.WebHistory;
 import javafx.scene.web.WebView;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.pentaho.di.core.EngineMetaInterface;
@@ -23,6 +30,7 @@ public class SpoonJavaFxBrowser implements TabItemInterface {
   FXCanvas canvas;
   WebView webView;
   Composite composite;
+  TextField urlBar;
 
   public SpoonJavaFxBrowser( Composite parent ) {
     composite = new Composite( parent, SWT.NONE );
@@ -34,15 +42,32 @@ public class SpoonJavaFxBrowser implements TabItemInterface {
 
   protected void init() {
     Group group = new Group();
-    final Scene scene = new Scene( group );
+    group.setAutoSizeChildren( true );
     webView = new WebView();
-    group.getChildren().add( webView );
     webView.setContextMenuEnabled( true );
-    final Button backButton = new Button( "Back" );
-    final Button forwardButton = new Button( "Forward" );
-    final TextField urlBar = new TextField();
-    group.getChildren().add( backButton );
+    final Button backButton = new Button( "<" );
+    backButton.setPrefSize(50, 20);
+    final Button forwardButton = new Button( ">" );
+    forwardButton.setPrefSize(50, 20);
+    final Button goButton = new Button( "Go" );
+    goButton.setPrefSize(50, 20);
+    urlBar = new TextField();
+    urlBar.setPrefSize( 400, 20 );
 
+    final HBox toolbar = new HBox();
+    toolbar.setAlignment( Pos.CENTER );
+    toolbar.getChildren().add( backButton );
+    toolbar.getChildren().add( forwardButton );
+    toolbar.getChildren().add( urlBar );
+    toolbar.getChildren().add( goButton );
+    toolbar.setPrefSize( 1000, 30 );
+
+    group.getChildren().add( toolbar );
+    group.getChildren().add( webView );
+    toolbar.setLayoutY( 0 );
+    webView.setLayoutY( 30 );
+
+    final Scene scene = new Scene( group );
     canvas.setScene( scene );
 
     scene.widthProperty().addListener( new ChangeListener<Object>()
@@ -50,6 +75,8 @@ public class SpoonJavaFxBrowser implements TabItemInterface {
       public void changed(ObservableValue<?> observable, Object oldValue, Object newValue)
       {
         Double width = (Double) newValue;
+        toolbar.setPrefWidth( width.doubleValue() );
+        urlBar.setPrefWidth( width.doubleValue() - 3*50 );
         webView.setPrefWidth( width.doubleValue() );
       }
     });
@@ -58,9 +85,27 @@ public class SpoonJavaFxBrowser implements TabItemInterface {
       public void changed(ObservableValue<?> observable, Object oldValue, Object newValue)
       {
         Double width = (Double) newValue;
-        webView.setPrefHeight( width.doubleValue() );
+        webView.setPrefHeight( width.doubleValue() - toolbar.getPrefHeight() );
       }
     });
+
+    backButton.setOnAction(event -> {
+      WebHistory history = webView.getEngine().getHistory();
+      if ( history.getCurrentIndex() > 0 ) {
+        history.go( -1 );
+        urlBar.setText( webView.getEngine().getLocation() );
+      }
+    });
+
+    forwardButton.setOnAction( event -> {
+      WebHistory history = webView.getEngine().getHistory();
+      if ( history.getCurrentIndex() < history.getEntries().size() - 1 ) {
+        history.go( 1 );
+        urlBar.setText( webView.getEngine().getLocation() );
+      }
+    });
+
+    goButton.setOnAction( event -> webView.getEngine().load( urlBar.getText() ));
 
   }
 
@@ -68,7 +113,8 @@ public class SpoonJavaFxBrowser implements TabItemInterface {
     return composite;
   }
 
-  public void setUrl(String url ) {
+  public void setUrl( String url ) {
+    urlBar.setText( url );
     webView.getEngine().load( url );
   }
 
